0eeb2afd1ae11f644e583c78c28471fcc187a18f
Ivar1999 <drednikeu@gmail.com>
2020-09-01 12:02:25 +0200

    I made some changes

--- First lession Zoom class/First lession Zoom class.cpp
+++ First lession Zoom class/First lession Zoom class.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-    std::cout << "Goodbye World";
+int age = 21;
+bool sw = false;
+char initial = 'm';
+std::string name = "Ivar";
+double pi = 3.14;
 
+
+int main() {
+
+	std::cout << sw;
+	std::cout << "Your age is : " << age << std::endl << "\n"; // double enter
+	std::cin >> age;
+	std::cout << "\tYou are young\n\n\n\n";
+	
+
+	/* this is commented out */
 }
